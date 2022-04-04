759b54d4b540fc5e21b7ee121da2c5b55b817dee
Gino Allison <77733677+Gnllsn@users.noreply.github.com>
2022-04-04 23:58:30 +0300

    integration listes plat , commandes clients

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,26 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { InscriptionComponent } from './components/inscription/inscription.component';
 
+import { HomeComponent } from './components/user/home/home.component';
+import { ListPlatComponent } from './components/user/list-plat/list-plat.component';
+import { CartComponent } from './components/user/cart/cart.component';
+
 const routes: Routes = [{
     path:'',
     component : LoginComponent
 },{
     path:'inscription',
     component:InscriptionComponent
+},{
+    path:'user',
+    component:HomeComponent,
+    children:[{
+        path:'',
+        component:ListPlatComponent,
+    },{
+        path:'cart',
+        component:CartComponent
+    }]
 }];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { InscriptionComponent } from './components/inscription/inscription.component';
+import { HomeComponent } from './components/user/home/home.component';
+import { ListPlatComponent } from './components/user/list-plat/list-plat.component';
+import { CartComponent } from './components/user/cart/cart.component';
 
 @NgModule({
-  declarations: [
+    declarations: [
     AppComponent,
     LoginComponent,
-    InscriptionComponent
-  ],
-  imports: [
+    InscriptionComponent,
+    HomeComponent,
+    ListPlatComponent,
+    CartComponent
+    ],
+    imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
